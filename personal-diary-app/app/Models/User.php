3a96b496e980bcb2eb230e50ee4e3c482bd2ed31
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-11 02:03:42 +0600

    One to Many Relationship on Note Added

--- personal-diary-app/app/Models/User.php
+++ personal-diary-app/app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    
+    public function note() {
+        return $this->hasMany(Note::class);
+    }
 }
