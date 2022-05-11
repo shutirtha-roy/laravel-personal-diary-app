6867882958755fcd5bd50da67aa5a3bf0a53c376
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-11 17:50:34 +0600

    Layout Updated

--- personal-diary-app/resources/views/layouts/app.blade.php
+++ personal-diary-app/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Personal Diary App - @yield('title')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -75,7 +75,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="py-4 container">
             @yield('content')
         </main>
     </div>
