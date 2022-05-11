3d21faedf0777e59c9b2f6eb100fea41c71387ad
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-11 18:02:17 +0600

    LoggedIn User Information Method Added

--- personal-diary-app/app/Http/Controllers/NoteController.php
+++ personal-diary-app/app/Http/Controllers/NoteController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
     
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function getLoggedInUserInfo()
+    {
+        return User::find(Auth::id());
+    }
+
+    public function create() 
+    {
+        $userName = $this->getLoggedInUserInfo()['name'];
+        return view('note.create', compact('userName'));
+    }
 }
