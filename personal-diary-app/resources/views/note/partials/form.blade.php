601e7b18588b057130eaacddda4e89927f0b03a1
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-13 03:17:50 +0600

    Public Opinion Added in Form

--- personal-diary-app/resources/views/note/partials/form.blade.php
+++ personal-diary-app/resources/views/note/partials/form.blade.php
@@ -10,6 +10,24 @@
         <textarea id="mytextarea" name="content"></textarea>
     </div>
 
+    <div class="mb-3">
+        <label for="title" class="form-label h3">Do you want to share it to the public?</label>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="public_opinion" id="flexRadioDefault1" value="yes">
+            <label class="form-check-label h5" for="flexRadioDefault1">
+                Yes
+            </label>
+            </div>
+            <div class="form-check">
+            <input class="form-check-input" type="radio" name="public_opinion" id="flexRadioDefault2"  value="no">
+            <label class="form-check-label h5" for="flexRadioDefault2">
+                No
+            </label>
+        </div>
+    </div>
+
     
+
+
     <button type="submit" class="btn btn-primary">Add Diary</button>
 </form>
