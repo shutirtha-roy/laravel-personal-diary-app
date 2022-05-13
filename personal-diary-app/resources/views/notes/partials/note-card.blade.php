064d00b8b5bca1ee56cbef6a80b060393c4d90fa
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-14 04:20:24 +0600

    View Card Information Added

---
+++ personal-diary-app/resources/views/notes/partials/note-card.blade.php
@@ -0,0 +1,11 @@
+<div class="col-12 col-sm-6 col-md-4 mt-3">
+    <div class="card">
+        <img src="{{ asset('images/'.$note->image) }}" class="card-img-top" style="width:100%; height: 35vh;">
+        <div class="card-body text-center">
+            <h5 class="card-title">{{ $note->title }}</h5>
+            <p class="card-text">{!! substr($note->content, 0, 80) !!}</p>
+            <a href="#" class="btn btn-primary">Edit</a>
+            <a href="#" class="btn btn-danger">Delete</a>
+        </div>
+    </div>
+</div>
