847304ea59b644a161887e597da5deb5c59e8603
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-14 21:15:47 +0600

    Welcome Blade Modified

--- personal-diary-app/resources/views/welcome.blade.php
+++ personal-diary-app/resources/views/welcome.blade.php
@@ -4,4 +4,16 @@
 
 @section('content')
     <h1 class="text-center">Welcome to Personal Diary </h1>
+    <h1 class="text-center h4 mb-5">Please check out your previously recorded (only public diaries)</h1>
+
+    <div class="row">
+        @forelse($allNotes as $note)
+            @include('notes.partials.note-card')
+        @empty
+            <h1>Empty</h1>
+        @endforelse
+    </div>
+
+    {{ $allNotes->links() }}
 @endsection
+
