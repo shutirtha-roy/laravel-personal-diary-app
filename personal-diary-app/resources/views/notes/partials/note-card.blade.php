cecc29d90f9e6be7215073eb2639ea4c8e7e40a3
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-15 00:05:12 +0600

    Categories shown in Note

--- personal-diary-app/resources/views/notes/partials/note-card.blade.php
+++ personal-diary-app/resources/views/notes/partials/note-card.blade.php
@@ -2,6 +2,14 @@
     <div class="card">
         <img src="{{ asset('images/'.$note->image) }}" class="card-img-top" style="width:100%; height: 35vh;">
         <div class="card-body text-center">
+
+            <span class="h4">Category: </span>
+            @forelse($note->category as $category)
+                <span class="h3"><button class="btn btn-info mb-3">{{ $category->category_type }}</button></span>
+            @empty
+                <h1></h1>
+            @endforelse
+
             <h5 class="card-title">{{ $note->title }}</h5>
             <p class="card-text">{!! substr($note->content, 0, 80) !!}</p>
 
