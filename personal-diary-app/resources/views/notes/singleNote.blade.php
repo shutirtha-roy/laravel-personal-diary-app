5fc9bae113d4016c73e170cd7ec65e5dac2ad65d
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-20 11:24:43 +0600

    Edit option condition added for Logged in User

--- personal-diary-app/resources/views/notes/singleNote.blade.php
+++ personal-diary-app/resources/views/notes/singleNote.blade.php
@@ -5,23 +5,24 @@
 @section('content')
 
         <div class="row">
-            <div class="col-12 col-sm-6 col-md-4 mt-3">
-                <div class="card">
-                    <img src="{{ asset('images/'.$note->image) }}" class="card-img-top" style="width:100%; height: 35vh;">
-                    <div class="card-body text-center">
             
-                        <span class="h4">Category: </span>
+            <div class="col-12">
+                <div class="text-center">
+                    <img src="{{ asset('images/'.$note->image) }}" class="card-img-top text-center" style="width:50%; height: 45vh;">
+                    <div class="text-center">
+            
+                        <span class="h3">Category: </span>
                         @forelse($note->category as $category)
                             <span class="h3"><button class="btn btn-info mb-3">{{ $category->category_type }}</button></span>
                         @empty
                             <h1></h1>
                         @endforelse
             
-                        <h5 class="card-title"><a href="{{ url('notes/' . $note->id) }}">{{ $note->title }}</a></h5>
-                        <p class="card-text">{!! $note->content !!}</p>
-            
+                        <h5 class="h1">{{ $note->title }}</h5>
+                        <h5 class="h4">{!! $note->content !!}</h5>
+
                         <div class="d-flex justify-content-center">
-                            @if(Route::is('notes.showSingleNote', ['id' => $note->id]))
+                            @if(Route::is('notes.showSingleNote', ['id' => $note->id]) && $note->user_id == Auth::id())
                                 @include('notes.partials.edit-delete-option')
                             @endif
                         </div>
