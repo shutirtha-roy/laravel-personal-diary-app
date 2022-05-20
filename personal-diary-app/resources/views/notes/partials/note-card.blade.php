02cd4d68e102329998fdc827505b0115933885c7
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-20 11:06:46 +0600

    Edit Delete Option Separated

--- personal-diary-app/resources/views/notes/partials/note-card.blade.php
+++ personal-diary-app/resources/views/notes/partials/note-card.blade.php
@@ -15,12 +15,7 @@
 
             <div class="d-flex justify-content-center">
                 @if(Route::is('notes.showNote'))
-                    <a href="{{ route('notes.editNote', ['id' => $note->id]) }}" class="btn btn-primary">Edit</a>
-                    <form action="{{ route('notes.noteDelete', ['id' => $note->id]) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" class="btn btn-danger" value="Delete!">
-                    </form>
+                    @include('notes.partials.edit-delete-option')
                 @endif
             </div>
             
