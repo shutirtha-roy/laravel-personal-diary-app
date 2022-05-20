dd2bfc0c2e72bb85a56004c6ac83f43fa4713620
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-20 11:06:16 +0600

    View of content Added

--- personal-diary-app/resources/views/notes/singleNote.blade.php
+++ personal-diary-app/resources/views/notes/singleNote.blade.php
@@ -5,7 +5,30 @@
 @section('content')
 
         <div class="row">
-            @include('notes.partials.note-card')
+            <div class="col-12 col-sm-6 col-md-4 mt-3">
+                <div class="card">
+                    <img src="{{ asset('images/'.$note->image) }}" class="card-img-top" style="width:100%; height: 35vh;">
+                    <div class="card-body text-center">
+            
+                        <span class="h4">Category: </span>
+                        @forelse($note->category as $category)
+                            <span class="h3"><button class="btn btn-info mb-3">{{ $category->category_type }}</button></span>
+                        @empty
+                            <h1></h1>
+                        @endforelse
+            
+                        <h5 class="card-title"><a href="{{ url('notes/' . $note->id) }}">{{ $note->title }}</a></h5>
+                        <p class="card-text">{!! $note->content !!}</p>
+            
+                        <div class="d-flex justify-content-center">
+                            @if(Route::is('notes.showSingleNote', ['id' => $note->id]))
+                                @include('notes.partials.edit-delete-option')
+                            @endif
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
 
 @endsection
