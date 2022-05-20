d8cb048462c2f1b985e6b907aaf8bf0d20d6ac60
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-20 11:28:49 +0600

    Code refactored

--- personal-diary-app/resources/views/notes/singleNote.blade.php
+++ personal-diary-app/resources/views/notes/singleNote.blade.php
@@ -4,32 +4,29 @@
 
 @section('content')
 
-        <div class="row">
-            
-            <div class="col-12">
-                <div class="text-center">
-                    <img src="{{ asset('images/'.$note->image) }}" class="card-img-top text-center" style="width:50%; height: 45vh;">
-                    <div class="text-center">
-            
-                        <span class="h3">Category: </span>
-                        @forelse($note->category as $category)
-                            <span class="h3"><button class="btn btn-info mb-3">{{ $category->category_type }}</button></span>
-                        @empty
-                            <h1></h1>
-                        @endforelse
-            
-                        <h5 class="h1">{{ $note->title }}</h5>
-                        <h5 class="h4">{!! $note->content !!}</h5>
+    <div class="col-12">
+        <div class="text-center">
+            <img src="{{ asset('images/'.$note->image) }}" class="card-img-top text-center" style="width:50%; height: 45vh;">
+            <div class="text-center">
+    
+                <span class="h3">Category: </span>
+                @forelse($note->category as $category)
+                    <span class="h3"><button class="btn btn-info mb-3">{{ $category->category_type }}</button></span>
+                @empty
+                    <h1></h1>
+                @endforelse
+    
+                <h5 class="h1">{{ $note->title }}</h5>
+                <h5 class="h4">{!! $note->content !!}</h5>
 
-                        <div class="d-flex justify-content-center">
-                            @if(Route::is('notes.showSingleNote', ['id' => $note->id]) && $note->user_id == Auth::id())
-                                @include('notes.partials.edit-delete-option')
-                            @endif
-                        </div>
-                        
-                    </div>
+                <div class="d-flex justify-content-center">
+                    @if(Route::is('notes.showSingleNote', ['id' => $note->id]) && $note->user_id == Auth::id())
+                        @include('notes.partials.edit-delete-option')
+                    @endif
                 </div>
+                
             </div>
         </div>
+    </div>
 
 @endsection
